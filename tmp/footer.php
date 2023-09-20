d86213cc3964dd2f13736796ad380069cc951f62
TJ Moore <tmoore@playqcr.com>
2023-09-19 21:00:23 -0700

    Removed pages no longer needed and links to those pages.

--- tmp/footer.php
+++ tmp/footer.php
@@ -1,4 +1,3 @@
-        <!-- <div class="container-fluid" style="background-color: #1b1145; height: 100px; color: #ffffff; margin-top: 20px; display: flex;"> -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
         <script type="text/javascript">
@@ -52,11 +51,6 @@
             <div class="text-center" style="color: #ffffff;">
                 Copyright &copy; <?php echo date('Y'); ?> <a href="https://teemor.com" target="_blank">TEEMOR eBusiness Solutions</a>. All rights reserved.
             </div>
-            <!-- </div> -->
         </footer>
-
-        
-        <!-- <script src="js/jquery-ui-min.js"></script>
-        <script src="js/jquery.js"></script> -->
     </body>
 </html>
